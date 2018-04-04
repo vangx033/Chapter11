b2878f96fdddc2e5d1c5f835de3a4832ac2f56fe
vangx033 <seeho.vang@hotmail.com>
2018-04-03 20:06:26 -0700

    Ch11script.sh

--- Ch11script.sh
+++ Ch11script.sh
@@ -2,3 +2,17 @@
 #Xee Ho Vnag
 #Week 12 - Chapter 11 script
 
+# Using quotes for examples 
+echo $255
+echo "$255"
+echo '$255'
+
+sleep 5
+
+# Using conditionals, if, then, else and case statements
+if [ "$1" = "Hi" ]; then
+   echo 'The first argument was "Hi"'
+else 
+   echo -n "The first argument was not "Hi" --"
+   echo "It was '"'$1'"'"
+fi
