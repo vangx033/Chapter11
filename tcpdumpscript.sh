2462643982cffe10447d5163cd85bacc5e322b1c
vangx033 <seeho.vang@hotmail.com>
2018-04-03 20:52:52 -0700

    tcpdumpscript.sh

--- tcpdumpscript.sh
+++ tcpdumpscript.sh
@@ -1,1 +1,18 @@
+#Xee Ho Vang
 #tcpdumpt script
+
+# Using -c from Daniel essay 
+ping -c 5 www.yahoo.com
+
+# Showing TCP packets 
+echo "Please press Ctrl C to exit"
+sudo tcpdump tcp
+sleep 5
+
+# Showing traffic from the IP you specified.
+echo "Please press Ctrl C to exit"
+sudo tcpdump host 8.8.8.8
+sleep 3
+
+# Show packets that are less then 64
+sudo tcpdump less 64
